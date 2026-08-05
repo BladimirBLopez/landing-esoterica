2fe402f456559f63530bd927cf18102876970e6b
BladimirBLopez <blopezbladimir@gmail.com>
2026-08-04 20:32:55 -0400

    Cambiar fuente cursiva del nombre a Sail

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Playfair_Display, Mrs_Saint_Delafield } from "next/font/google";
+import { Playfair_Display, Sail } from "next/font/google";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -7,7 +7,7 @@ const playfair = Playfair_Display({
   subsets: ["latin"],
 });
 
-const alexBrush = Mrs_Saint_Delafield({
+const alexBrush = Sail({
   variable: "--font-script",
   weight: "400",
   subsets: ["latin"],
