51c9abc0a9fd6bdacbbcd0111f9bd111b7eb7be7
BladimirBLopez <blopezbladimir@gmail.com>
2026-08-06 10:38:22 -0400

    chore: actualizar foto del Hero a maestro-juan4

--- app/page.tsx
+++ app/page.tsx
@@ -178,7 +178,7 @@ export default function Home() {
           </p>
           <div className="relative -mt-16 h-[30rem] w-80 sm:h-[34rem] sm:w-96">
             <Image
-              src="https://res.cloudinary.com/dkq95jus0/image/upload/maestro-juan3"
+              src="https://res.cloudinary.com/dkq95jus0/image/upload/maestro-juan4"
               alt="Maestro Juan Santiago"
               fill
               className="object-contain"
